18336f2c499d1348cd710b82f3caf38b2d0f30fa
Jane Kim <jane.kim0717@gmail.com>
2022-02-28 12:38:46 -0500

    First 3 deliverables

--- src/components/MoreButton.js
+++ src/components/MoreButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function MoreButton(props) {
-  return <button onClick={/* Fill me in! */ null}>More sushi!</button>;
+function MoreButton({handleMoreSushis}) {
+  return <button onClick={handleMoreSushis}>More sushi!</button>;
 }
 
 export default MoreButton;
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ sushis }) {
+
+  const [sushiIndex, setSushiIndex] = useState(0);
+
+  const sushiComponents = sushis
+  .slice(sushiIndex, sushiIndex + 4)
+  .map((sushi) => 
+    <Sushi key = {sushi.id} sushi ={sushi}/>
+  )
+
+  function handleMoreSushis() {
+    setSushiIndex((sushiIndex) => sushiIndex + 4)
+  }
+
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiComponents}
+      <MoreButton handleMoreSushis = {handleMoreSushis} />
     </div>
   );
 }
